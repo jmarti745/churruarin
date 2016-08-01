aa1027aaa1aa46ff421e8553c10ec3421599ec9b
abelbour <abelbour2k@yahoo.com>
2016-08-01 20:08:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,35 @@ app.config(function($routeProvider){
 
       $routeProvider
           .when('/',{
-                templateUrl: 'home.html'
+                templateUrl: 'inicio.html'
           })
-          .when('/about',{
-                templateUrl: 'about.html'
+          .when('/predicacion',{
+                templateUrl: 'predicacion/index.html'
           });
-
-
+          .when('/salidas',{
+                templateUrl: 'predicacion/salidas.html'
+          });
+          .when('/territorios',{
+                templateUrl: 'predicacion/territorios.html'
+          });         
+          .when('/informe',{
+                templateUrl: 'predicacion/informe.html'
+          });    
+          .when('/reuniones',{
+                templateUrl: 'reuniones/index.html'
+          });
+          .when('/vivo',{
+                templateUrl: 'reuniones/vivo.html'
+          }); 
+          .when('/finde',{
+                templateUrl: 'reuniones/finde.html'
+          });           
+          .when('/semana',{
+                templateUrl: 'reuniones/semana.html'
+          });
+          .when('/programa',{
+                templateUrl: 'reuniones/programa.html'
+          });           
 });
 
 
